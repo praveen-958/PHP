a16fbe7645e8e94eb49d0cacf9c172c5776bab61
Praveen <praveenchouhan7272@gmail.com>
2024-02-06 21:40:27 +0530

    intial commit

---
+++ array.php
@@ -0,0 +1,20 @@
+<?php
+
+$nums = array("mango", "apple", "banana", "graps"); //this is how you can define an array
+
+
+// for($i =0 ; $i < count($nums); $i++){
+
+//     echo $nums[$i];
+//     echo "\n";
+// }
+
+
+$i= 0;
+while($i<count($nums)){
+
+    echo $nums[$i];
+    echo "\n";
+    $i++;
+}
+?>
---
+++ greates.php
@@ -0,0 +1,26 @@
+<?php
+
+$a = 18;
+$b = 72;
+$c = 3;
+
+if($a>$b){
+    if($a>$c){
+        echo $a;
+    }
+    else{
+        echo $c;
+    }
+
+}
+else{
+    if($b>$c){
+        echo $b;
+    }
+    else{
+        echo $c;
+    }
+}
+
+
+?>
